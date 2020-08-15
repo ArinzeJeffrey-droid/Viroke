a6f431d0afabb6904b2fa9bc5f68db956a8c4fa0
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-15 19:10:03 +0100

    Updates

---
+++ precache-manifest.63436b75edfbc38e958f03202c3a8819.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c431b853479201cd35e01d206ae8c75",
+    "url": "/Viroke/index.html"
+  },
+  {
+    "revision": "5381f1e5cd5c29cc9bfa",
+    "url": "/Viroke/static/css/main.887a69b3.chunk.css"
+  },
+  {
+    "revision": "24a03fb582c6ca837c16",
+    "url": "/Viroke/static/js/2.131777b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Viroke/static/js/2.131777b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5381f1e5cd5c29cc9bfa",
+    "url": "/Viroke/static/js/main.eabe46b6.chunk.js"
+  },
+  {
+    "revision": "2027969519013ec552dd",
+    "url": "/Viroke/static/js/runtime-main.ebafb6e2.js"
+  },
+  {
+    "revision": "63911ea82782196af36696e44ddc66c1",
+    "url": "/Viroke/static/media/Group-292@2x.63911ea8.png"
+  },
+  {
+    "revision": "59dff3a45aed16e51f206e00a1abe6bb",
+    "url": "/Viroke/static/media/Home-1@2x.59dff3a4.png"
+  },
+  {
+    "revision": "305637c83b5a52a9fc2bd5a0ba1f5bd5",
+    "url": "/Viroke/static/media/Session Details@2x.305637c8.png"
+  },
+  {
+    "revision": "457d41aa0b6a02c13e921e7035994080",
+    "url": "/Viroke/static/media/Sessions Screen@2x.457d41aa.png"
+  },
+  {
+    "revision": "391f31b135fcbca28761051f5f88d8f7",
+    "url": "/Viroke/static/media/Wallet@2x.391f31b1.png"
+  },
+  {
+    "revision": "5a0872b906daea2c110dc6edbea725ec",
+    "url": "/Viroke/static/media/appstore@2x.5a0872b9.png"
+  },
+  {
+    "revision": "2ced755f4454495714967894f863ec81",
+    "url": "/Viroke/static/media/google-play@2x.2ced755f.png"
+  },
+  {
+    "revision": "1a790327e73941d3f7047bc000cfe0a4",
+    "url": "/Viroke/static/media/nav_bg2.1a790327.png"
+  },
+  {
+    "revision": "dbaad6fefefc59c550147fc0dacb4f2e",
+    "url": "/Viroke/static/media/nav_cusBg.dbaad6fe.PNG"
+  }
+]);

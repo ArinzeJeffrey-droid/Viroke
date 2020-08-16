a4295580831a78f80eee1afdd1e0e10b2b379eaf
arinzeJeffrey <nnamenearinze@gmail.com>
2020-08-16 16:51:35 +0100

    made the app responsive

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,42 +4,45 @@ import "./footer.styles.scss"
 
 const Footer = () => {
     return (
-        <div className="footer__start">
+        <div className="footer__container container-fluid">
             <div className="footer__top">
-                <div className="update">
-                    <h1>Need updates on latest jobs?</h1>
-                    <p>sign up to get the latest updates</p>
-                </div>
-                <div className="update__form">
-                    <form action="">
-                        <input type="email" name="" placeholder="Enter Valid Email Address"/>
-                        <button>Send</button>
-                    </form>
+                <div className="footer__inner row">
+                    <div className="need__update col-md-6">
+                        <h1>Need updates on latest jobs?</h1>
+                        <p>sign up to get the latest updates</p>
+                    </div>
+                    <div className="update__form col-md-6">
+                        <form action="">
+                        <input type="email" className="form-control" placeholder="Enter Valid Email"/>
+                        <button className="btn">Send</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-            <div className="footer__bottom">
-                <div className="info">
-                    <h3>Viroke</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur <br/> adipisicing elit. Aperiam qui ducimus <br/> temporibus harum laboriosam quidem <br/> doloribus facere molestias? Minima, <br/> reprehenderit!</p>
-                </div>
-                <div className="info__list">
-                    <h3>The Company</h3>
-                    <ul>
-                        <li>About</li>
-                        <li>Success Stories</li>
-                        <li>Contact Us</li>
-                        <li>FAQ</li>
-                    </ul>
-                </div>
-                <div className="info__list">
+            <div className="footer__bottom mt-5">
+                <div className="inner__bottom row">
+                    <div className="footer-box col-md-4">
+                        <h3>Viroke</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut est earum voluptate nisi sint quidem? Vitae distinctio non ex fugit dignissimos, maiores veritatis quidem quisquam ratione voluptatem perferendis pariatur sunt.</p>
+                    </div>
+                    <div className="footer-box col-md-4">
+                        <h3>The Company</h3>
+                        <ul>
+                            <li>About</li>
+                            <li>Success Stories</li>
+                            <li>About Us</li>
+                            <li>FAQ</li>
+                        </ul>
+                    </div>
+                    <div className="footer-box col-md-4">
                     <h3>Connect</h3>
-                    <ul>
-                        <li>Link</li>
-                        <li>Refer a friend</li>
-                    </ul>
+                        <ul>
+                            <li>Link</li>
+                            <li>Refer A Friend</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <footer></footer>
         </div>
     );
 }
--- src/components/landing-img/landing-img.component.jsx
+++ src/components/landing-img/landing-img.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import "./landing-img.styles.scss"
 import ScreenShot from "../../assets/image/Group-292@2x.png"
 import AppleLogo from "../../assets/image/appstore@2x.png"
@@ -6,29 +7,26 @@ import GoogleLogo from "../../assets/image/google-play@2x.png"
 
 
 const LandingImg = () => {
-    return (  
-        <div>
-            <div className="container">
-                <div className="img__container">
-                    <img src={ScreenShot} alt=""/>
-                </div>
+    return (
+        <div className="container-fluid overlay__section">
+            <div className="image__hidden">
+                <img src={ScreenShot} alt=""/>
             </div>
-            <div className="overlap">
-                <div className="box"></div>
-                <div className="ad__box">
-                    <div className="host">
-                        <h1>Want to host a session?</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum esse voluptatibus ut tempora sapiente adipisci, consequuntur quos </p>
-                        <button>Request An Invite</button>
+            <div className="overlay__box container-fluid">
+                <div className="overlay__inner row">
+                    <div className="host-session col-md-6">
+                        <h1>Want to host a session</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sint dolorem doloribus dicta nam neque provident excepturi ut, fuga possimus.</p>
+                        <button className="btn">Request an invite</button>
                     </div>
-                    <div className="get__started">
-                        <h1>Ready To Get Started?</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum esse voluptatibus ut tempora sapiente adipisci, consequuntur quos </p>
-                        <div className="app__box">
-                            <div className="boxes">
+                    <div className="updates col-md-6">
+                        <h1>Ready to get started</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam sint dolorem doloribus dicta nam neque provident excepturi ut, fuga possimus.</p>
+                        <div className="app">
+                            <div className="box">
                                 <img src={GoogleLogo} alt=""/>
                             </div>
-                            <div className="boxes">
+                            <div className="box">
                                 <img src={AppleLogo} alt=""/>
                             </div>
                         </div>
